f95743ac0fc09b58879d23bc4801e997f47e3cad
cowik <cowike@gmail.com>
2021-01-29 19:31:54 +0700

    edit route imageCourse

--- src/routes/imageCourses.js
+++ src/routes/imageCourses.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const imageCoursesHandler = require('./handler/imagecourses');
+const imageCoursesHandler = require('./handler/imageCourses');
 
 router.post('/', imageCoursesHandler.create)
 router.delete('/:id', imageCoursesHandler.destroy)
